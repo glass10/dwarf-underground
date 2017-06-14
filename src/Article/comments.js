86ef2cd59901d7b7eb2408c6ee454f80bb928a96
Cameron Glass <glass10@purdue.edu>
2017-06-14 14:35:16 -0400

    Update state with Comments

--- src/Article/comments.js
+++ src/Article/comments.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 class Comments extends Component{
+    constructor(){
+        super()
+        this.state = {
+            comment: ''
+        }
+        this.updateComment = this.updateComment.bind(this);
+    }
+    updateComment(ev){
+        this.setState({
+            comment: ev.target.value
+        }, () => console.log(this.state))
+    }
     render(){
         return(
             <div className="comments">
-                <textarea></textarea>
+                <textarea value={this.state.comment} onChange={this.updateComment}></textarea>
                 <button className="button">Submit Comment</button>
             </div>
         )
